9e2eebe0068b94a74ba0b64cf2a895332b644580
fireblizzard <mikel.iglesias@maristak.net>
2020-03-25 23:09:39 +0100

    Increased Barney's HP

--- dlls/barney.cpp
+++ dlls/barney.cpp
@@ -28,6 +28,8 @@
 #include	"weapons.h"
 #include	"soundent.h"
 
+#define		BARNEY_EXTRA_HEALTH	30.0
+
 //=========================================================
 // Monster's Anim Events Go Here
 //=========================================================
@@ -413,7 +415,7 @@ void CBarney :: Spawn()
 	pev->solid			= SOLID_SLIDEBOX;
 	pev->movetype		= MOVETYPE_STEP;
 	m_bloodColor		= BLOOD_COLOR_RED;
-	pev->health			= gSkillData.barneyHealth;
+	pev->health			= gSkillData.barneyHealth + BARNEY_EXTRA_HEALTH; // TODO: refactor
 	pev->view_ofs		= Vector ( 0, 0, 50 );// position of the eyes relative to monster's origin.
 	m_flFieldOfView		= VIEW_FIELD_WIDE; // NOTE: we need a wide field of view so npc will notice player and say hello
 	m_MonsterState		= MONSTERSTATE_NONE;
