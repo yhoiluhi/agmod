233fec17e0eda0719f7a90bdf264e9e9fd7d0754
fireblizzard <16053926+fireblizzard@users.noreply.github.com>
2022-01-03 19:18:39 +0100

    Allow for longer cvars to be voted

--- dlls/agvote.cpp
+++ dlls/agvote.cpp
@@ -171,7 +171,7 @@ bool AgVote::HandleCommand(CBasePlayer* pPlayer)
                 m_sValue2 = CMD_ARGV(2);
         }
 
-        if (m_sVote.size() && 32 > m_sVote.size() && 32 > m_sValue.size())
+        if (m_sVote.size() && 64 > m_sVote.size() && 32 > m_sValue.size())
         {
             //Check map
             if (FStrEq(m_sVote.c_str(), "agmap") ||
